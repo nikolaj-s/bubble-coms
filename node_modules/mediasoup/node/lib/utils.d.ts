72b64d73846e0cd33962269accc081f33586a27a
Nikolaj <niko.sage@gmail.com>
2022-07-07 10:07:57 -0700

    ignore node

--- node_modules/mediasoup/node/lib/utils.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Clones the given object/array.
- */
-export declare function clone(data: any): any;
-/**
- * Generates a random positive integer.
- */
-export declare function generateRandomNumber(): number;
-//# sourceMappingURL=utils.d.ts.map
