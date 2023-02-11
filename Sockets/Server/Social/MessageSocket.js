6f86e437a7e9d291e594567072f3e72a99ee878e
Nikolaj <niko.sage@gmail.com>
2023-02-11 11:02:53 -0800

    added support for vimeo and links alongside iframes

--- Sockets/Server/Social/MessageSocket.js
+++ Sockets/Server/Social/MessageSocket.js
@@ -67,38 +67,60 @@ const MessageSocket = async (socket, data, channelList, cb) => {
 
         let t;
 
+        let twitter;
+
         if (data.content.text.includes('https')) {
             for (const text of data.content.text.split(' ')) {
                 if (text.includes('redgif')) {
                 
                     iFrame = "https://redgifs.com/ifr/" + (text.split('redgifs.com/')[1]?.includes('watch') ? text.split('redgifs.com/')[1]?.split('watch/')[1].toLowerCase() : text.split('redgifs.com/')[1]?.split('-')[0].toLowerCase());
-                
+                    
+                    link = text;
+
                 } else if (text.includes('youtu')) {
     
                     iFrame = "https://www.youtube.com/embed/" + (text.split('/')[3].includes('watch?') ? text.split('/')[3].split('watch?v=')[1].split('&')[0] : text.split('/')[3]);
-    
+                    
+                    link = text;
+                    
                 } else if (text.includes('pornhub')) {
     
                     iFrame = "https://www.pornhub.com/embed/" + (text.split('viewkey=')[1])
-    
+                    
+                    link = text;
+
                 } else  if (text.includes('xvideos')) {
     
                     iFrame = "https://www.xvideos.com/embedframe/" + (text.split('video')[1].split('/')[0]);
-    
+                    
+                    link = text;
+
                 } else if (text.includes('reddit')) {
     
                     iFrame = "https://www.redditmedia.com/r/" + (text.split('r/')[1].split('?utm_')[0] + "?ref_source=embed&amp;ref=share&amp;embed=true&amp;theme=dark")
-    
+                    
+                    link = text;
+
                 } else if (text.includes('steampowered')) {
     
                     iFrame = "https://store.steampowered.com/widget/" + (text.split('app/')[1].split('/')[0]);
-    
+                    
+                    link = text;
+
                 } else if (text.includes('twitter')) {
                     
                     twitter = text.split('status/')[1].split('?')[0];
                     
+                    link = text;
+
+                } else if (text.includes('vimeo')) {
+                    
+                    iFrame = "https://player.vimeo.com/video/" + text.split('com/')[1];
+                    
+                    link = text;
+
                 } else if (text.includes('https')) {
-    
+
                     link = text;
                 
                 } else {
@@ -116,6 +138,7 @@ const MessageSocket = async (socket, data, channelList, cb) => {
             video: video,
             link: link,
             iFrame: iFrame,
+            twitter: twitter,
             local_id: data.content.local_id,
             date: new Date(),
         }
