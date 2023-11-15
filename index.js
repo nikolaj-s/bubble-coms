a75cdd45c58f057004a4925154ca73ac78b76499
Nikolaj <niko.sage@gmail.com>
2023-11-15 14:29:24 -0800

    cors default

--- index.js
+++ index.js
@@ -36,13 +36,11 @@ const options = {
     cert: fs.readFileSync(path.join(__dirname + "/server/", config.sslCrt), 'utf-8')
 }
 
-App.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", ["http://10.0.0.187:3000", "10.0.0.187:3000", "http://localhost:3000", "https://bubblenetwork.netlify.app", "https://thebubble.network"]);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+const whitelist = ['http://localhost:3000', 'http://localhost']
 
-App.use(cors({orgin: ['http://10.0.0.187:3000', '10.0.0.187:3000', 'localhost:3000', 'http://localhost:3000', 'localhost'], exposedHeaders: ["auth_token", "API_KEY"], methods: ["GET", "POST", "PUT", "DELETE"]}));
+
+
+App.use(cors());
 
 const server = https.createServer(options, App);
 
