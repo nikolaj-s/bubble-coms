e6d5cae27a90f8e2909d9ce2ca40830808588d62
Nikolaj <niko.sage@gmail.com>
2024-03-11 12:39:28 -0700

    added verification level to fetching messages

--- Sockets/Server/Social/FetchMessages.js
+++ Sockets/Server/Social/FetchMessages.js
@@ -1,4 +1,5 @@
 const { MessageSchema } = require("../../../Schemas/Message/MessageSchema");
+const { ServerSchema } = require("../../../Schemas/Server/Server/ServerSchema");
 
 const FetchMessages = async (socket, data, cb) => {
     try {
@@ -6,6 +7,30 @@ const FetchMessages = async (socket, data, cb) => {
         let d;
 
         let nsfw;
+
+        let authorized = true;
+
+        const server = await ServerSchema.findOne({_id: socket.current_server});
+
+        if (!server) return cb({error: true, errorMessage: "Validation Error"});
+
+        let member = server.members.find(u => u.username === socket.AUTH.username);
+
+        if (!member) return cb({error: true, errorMessage: "Validation Error"});
+
+        if (data.channel_id) {
+            const channel = server.channels.find(c => String(c._id) === String(data.channel_id));
+
+            if (!channel) return cb({error: true, errorMessage: "Validation Error"})
+
+            if (channel?.locked_channel) {
+                const checkIfAuth = channel.auth_users.findIndex(u => u === String(member?._id));
+
+                if (checkIfAuth === -1) authorized = false;
+            }
+        }
+
+        if (authorized === false) return cb({error: true, errorMessage: "You are not authorized to look in this channel"});
         
         if (data.type === 'screenshots') {
             d = await MessageSchema.find({screen_shot: true, server_id: socket.current_server}).sort({"date": -1}).limit(data.count);
