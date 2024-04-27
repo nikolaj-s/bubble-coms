6af4fb6e5855161219af36c8fea11f76b4ed9798
Nikolaj <niko.sage@gmail.com>
2024-04-27 11:50:50 -0700

    fixed gif frame data not transfering on channel join

--- Sockets/Server/ChannelSocket/UserJoinsChannelSocket.js
+++ Sockets/Server/ChannelSocket/UserJoinsChannelSocket.js
@@ -2,6 +2,9 @@
 const Peer = require("../../../MediaSoup/Peer/Peer");
 
 const Channel = require("../../../MediaSoup/Channel/Channel");
+const { AccountSchema } = require("../../../Schemas/Account/AccountSchema");
+
+
 
 const UserJoinsChannelSocket = async (socket, data, io, channelList, getMediasoupWorker, cb) => {
     try {
@@ -17,11 +20,7 @@ const UserJoinsChannelSocket = async (socket, data, io, channelList, getMediasou
 
         if (channelList.get(channel_id)?.peers?.has(socket.id)) return cb({error: true, errorMessage: "you are already in this channel"});
 
-        channelList.get(channel_id).addPeer(new Peer(socket.id, socket.AUTH.username, data.user));
-
-        socket.channel_id = channel_id;
-
-        socket.join(channel_id);
+        const user = await AccountSchema.findOne({username: socket.AUTH.username});
 
         const data_to_send = {
             _id: data.user._id,
@@ -31,8 +30,16 @@ const UserJoinsChannelSocket = async (socket, data, io, channelList, getMediasou
             user_banner: data.user.user_banner,
             display_name: data.user.display_name,
             mirror_web_cam: data.user.mirror_web_cam,
-            color: data.user.color
+            color: data.user.color,
+            user_image_gif_frame: user.user_image_gif_frame,
+            user_banner_gif_frame: user.user_banner_gif_frame
         }
+
+        channelList.get(channel_id).addPeer(new Peer(socket.id, socket.AUTH.username, data_to_send));
+
+        socket.channel_id = channel_id;
+
+        socket.join(channel_id);
 
         socket.to(data.server_id).emit('user joins channel', data_to_send);
 
