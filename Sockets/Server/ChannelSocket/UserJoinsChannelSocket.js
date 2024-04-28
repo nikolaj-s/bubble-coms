a0d7e1f605fdf1d4fa988c0ce1bceeb02f42b913
Nikolaj <niko.sage@gmail.com>
2024-04-28 09:07:48 -0700

    removed await line in joining channel that led to unexpected issues

--- Sockets/Server/ChannelSocket/UserJoinsChannelSocket.js
+++ Sockets/Server/ChannelSocket/UserJoinsChannelSocket.js
@@ -2,9 +2,6 @@
 const Peer = require("../../../MediaSoup/Peer/Peer");
 
 const Channel = require("../../../MediaSoup/Channel/Channel");
-const { AccountSchema } = require("../../../Schemas/Account/AccountSchema");
-
-
 
 const UserJoinsChannelSocket = async (socket, data, io, channelList, getMediasoupWorker, cb) => {
     try {
@@ -20,19 +17,9 @@ const UserJoinsChannelSocket = async (socket, data, io, channelList, getMediasou
 
         if (channelList.get(channel_id)?.peers?.has(socket.id)) return cb({error: true, errorMessage: "you are already in this channel"});
 
-        const user = await AccountSchema.findOne({username: socket.AUTH.username});
-
         const data_to_send = {
-            _id: data.user._id,
             channel: {_id: data.channel_id},
-            username: socket.AUTH.username,
-            user_image: data.user.user_image,
-            user_banner: data.user.user_banner,
-            display_name: data.user.display_name,
-            mirror_web_cam: data.user.mirror_web_cam,
-            color: data.user.color,
-            user_image_gif_frame: user?.user_image_gif_frame,
-            user_banner_gif_frame: user?.user_banner_gif_frame
+            ...data.user
         }
 
         channelList.get(channel_id).addPeer(new Peer(socket.id, socket.AUTH.username, data_to_send));
