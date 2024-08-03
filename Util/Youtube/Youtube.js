56de5192b0efd892bbe9f88567a9e13f21c03366
Nikolaj <niko.sage@gmail.com>
2024-08-03 13:54:29 -0700

    fixed youtube video parsing issue

--- Util/Youtube/Youtube.js
+++ Util/Youtube/Youtube.js
@@ -3,8 +3,25 @@ const youtube = require('scrape-youtube')
 
 const FetchYoutubeVideo = async (query) => {
     try {
-        console.log(query)
-        const data = await youtube.search(query).then(res => {
+        
+        if (!query || query.length === 0) return {error: true, message: "Invalid query"};
+
+        let stripped_of_params = query.split('&')[0];
+
+        let video_id;
+
+        if (query?.includes('?v=')) {
+            video_id = stripped_of_params?.split('?v=')[1];
+        } else {
+            let segments = stripped_of_params?.split('/');
+
+            video_id = segments[segments.length - 1]?.split('?')[0];
+        }
+
+        if (!video_id) video_id = query;
+
+        const data = await youtube.search(video_id).then(res => {
+            console.log(res)
             console.log(res.videos)
             return res.videos[0];
         })
